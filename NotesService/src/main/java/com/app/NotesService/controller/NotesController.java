d19d495a2f898ceb7e6b68f04841af85ab37e272
vgajjar <vgajjar22@gmail.com>
2024-06-11 15:21:24 +0100

    added a basic test method

--- NotesService/src/main/java/com/app/NotesService/controller/NotesController.java
+++ NotesService/src/main/java/com/app/NotesService/controller/NotesController.java
@@ -1,4 +1,13 @@
 package com.app.NotesService.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NotesController {
+
+    @GetMapping
+    public String getHome(){
+        return "Home";
+    }
 }
