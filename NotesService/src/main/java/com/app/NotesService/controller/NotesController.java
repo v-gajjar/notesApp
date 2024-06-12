bc4c6fc75a94deaf76e5480fb87cd3badbd2fe9f
vgajjar <vgajjar22@gmail.com>
2024-06-12 12:36:37 +0100

    add definition for the Note object and create a rest url to test it

--- NotesService/src/main/java/com/app/NotesService/controller/NotesController.java
+++ NotesService/src/main/java/com/app/NotesService/controller/NotesController.java
@@ -1,5 +1,6 @@
 package com.app.NotesService.controller;
 
+import com.app.NotesService.model.Note;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,18 @@ public class NotesController {
     public String getHome(){
         return "Home";
     }
+
+    @GetMapping("/note")
+    public Note getNote(){
+
+        Note note = new Note();
+
+        note.setId(Integer.toUnsignedLong(1));
+        note.setTitle("A quick test note");
+        note.setContent("Hello, World!");
+
+        return note;
+    }
+
+
 }
--- NotesService/src/main/java/com/app/NotesService/model/Note.java
+++ NotesService/src/main/java/com/app/NotesService/model/Note.java
@@ -1,4 +1,50 @@
 package com.app.NotesService.model;
 
 public class Note {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public Note() {
+    }
+
+    public Note(String title, Long id, String content) {
+        this.title = title;
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
